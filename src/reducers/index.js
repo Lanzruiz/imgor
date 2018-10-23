bd2b74bd796500901e68b4275ad351e6872d1e4b
serhii <mikhajlov87@gmail.com>
2018-10-23 13:33:56 +0300

    add weeks counter

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import { reducer as formReducer } from 'redux-form';
 import steps from './steps';
+import weeks from './weeks';
 
 export default combineReducers({
   steps,
+  weeks,
   form: formReducer,
 });
