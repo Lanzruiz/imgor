b54218155556031df8e077f27db8d05ee020f2c1
Courtney Adesile <ca@largeinc.com>
2018-12-05 13:14:36 +0000

    deploy

---
+++ deploy.sh
@@ -0,0 +1,11 @@
+# Add remote repo
+git remote add cs git@bitbucket.org:cs-bitbucket/imgor-react.git
+
+# Pull from CS remote
+git pull cs master
+
+# Pull from ATL remote
+git pull origin master
+
+# Push to remote ATL repo
+git push origin master
