f875c9e585c328f007c5dfd535b72ddbd7042236
Courtney Adesile <cadesile@atlasnetwork.com>
2019-03-22 10:33:55 +0000

    event name

--- src/helpers/GTMService.js
+++ src/helpers/GTMService.js
@@ -46,7 +46,7 @@ function pushToDataLayer(data) {
 function gtmStateChangeHandler(pageType = '', state) {
   
   const gtmParams = {
-    event: 'newPageData',
+    event: 'newPage',
     newPageData: {
       params: {
         age: state.age,
