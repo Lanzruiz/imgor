2ab03ee1968dd9717bcd06c85bccfd6094033e08
Brian Schneider <Brian.Schneider@img.com>
2019-04-19 11:08:58 -0400

    updating keys and scrolling

--- src/containers/StepTwo/index.js
+++ src/containers/StepTwo/index.js
@@ -6,7 +6,7 @@ import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import cx from 'classnames';
 import moment from 'moment';
-import scrollToComponent from 'react-scroll-to-component';
+//import scrollToComponent from 'react-scroll-to-component';
 import isEqual from 'lodash/isEqual';
 // Components
 import Header from '../../components/Header';
@@ -146,9 +146,9 @@ class StepTwo extends React.Component {
     this.setDefaultState();
   }
 
-  scrollToCurrentComponent = () => {
+  //scrollToCurrentComponent = () => {
     //scrollToComponent(this.stepTwo.current, { offset: -200, align: 'middle', duration: 1000 });
-  };
+  //};
 
   render() {
     const { data, weeksCounter, sport, selectedDate, boarding } = this.props;
