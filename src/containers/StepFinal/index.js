dc80aa2faa1bd8d5035fb87165a0efc393ba944c
Brian Schneider <Brian.Schneider@img.com>
2019-05-13 14:29:49 -0400

    udpating travel cta text

--- src/containers/StepFinal/index.js
+++ src/containers/StepFinal/index.js
@@ -124,13 +124,6 @@ class StepFinal extends React.Component {
     
     return (
       <ScreenClassRender render={(screenClass) => {
-        let isMobile = false;
-        let isTablet = false;
-        if (isEqual(screenClass, 'sm') || isEqual(screenClass, 'xs')) {
-          isMobile = true;
-        } else if (isEqual(screenClass, 'md')) {
-          isTablet = true;
-        }
         return (
           <AOSFadeInContainer className="step-final" id="step-7">
             <Container>
@@ -330,7 +323,12 @@ class StepFinal extends React.Component {
 
               {!isBusinessTypeForAdult && (
               <Row>
-                <Col sm={12} md={4} lg={4} style={{ padding: '15px' }}>
+                <Col
+                  sm={12}
+                  md={4}
+                  lg={4}
+                  style={{ padding: '15px' }}
+                >
                   <Card
                     buttonBlock={false}
                     cardHeader={<LocaleString stringKey="step_final.guardian_information" />}
