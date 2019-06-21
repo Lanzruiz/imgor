e4fbaeafd602ef94a81046ce3b9021fc809de0d0
Brian Schneider <Brian.Schneider@img.com>
2019-06-21 14:42:45 -0400

    udpating step three

--- src/components/GameChangerCard/index.js
+++ src/components/GameChangerCard/index.js
@@ -2,7 +2,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 // Components
-import Card, { CardContentRow, CardContentCol, ImagePlus } from '../Card';
+import CardProduct, { CardContentRow, CardContentCol } from '../CardProduct';
 import LocaleString from '../../components/LocaleString';
 // Images
 import footballGameChanger from '../../assets/img/football-game-changer.png';
@@ -12,7 +12,7 @@ import './styles.scss';
 const GameChangerCard = (props) => {
   const { selectedId, onClick, onRemove, price, label, id, header, soldOut, displayViaLabel } = props;
   return (
-    <Card
+    <CardProduct
       cardHeader={<LocaleString stringKey="individualized_sport_training" />}
       color="red"
       header={header}
@@ -28,45 +28,24 @@ const GameChangerCard = (props) => {
       size="large"
       via={displayViaLabel}
       soldOut={soldOut}
+      overRideButtonName={header}
     >
-    <CardContentRow>
+      <CardContentRow>
         <CardContentCol>
-          <div className="game-changer-card__container">
-            <div className="game-changer-card__column game-changer-card__column--center game-changer-card__column--lh-20">
-              <span className="game-changer-card__column--core game-changer-card__column--lh-16">
-                <LocaleString stringKey="core" />
-              </span>
-              <span className="game-changer-card__column--core-training game-changer-card__column--lh-16">
-                <LocaleString stringKey="training" />
-              </span>
-              <span className="game-changer-card__column--ampersand">&</span>
-              <span className="game-changer-card__column--breakthrough game-changer-card__column--lh-16">
-                <LocaleString stringKey="breakthrough" />
-              </span>
-              <span className="game-changer-card__column--breakthrough-training game-changer-card__column--lh-16">
-                <LocaleString stringKey="training" />
-              </span>
-            </div>
-            <ImagePlus className="game-changer-card__image" />
-          </div>
-        </CardContentCol>
-        <CardContentCol>
-          <div className="game-changer-card__container">
-            <div className="game-changer-card__column game-changer-card__column--center game-changer-card__column--lh-16">
-              <span className="game-changer-card__column--daily-training-concentration">
-                <LocaleString stringKey="daily-training-concentration" />
-              </span>
-              <span className="game-changer-card__column--or">
-                -<LocaleString stringKey="or" />-
-              </span>
-              <span className="game-changer-card__column--education-class">
-                <LocaleString stringKey="education-class" />
-              </span>
-            </div>
-          </div>
+          <ul className="product-list">
+            <li>
+              <LocaleString stringKey="step_three.gamechanger.bullet.1" />
+            </li>
+            <li>
+              <LocaleString stringKey="step_three.gamechanger.bullet.2" />
+            </li>
+            <li>
+              <LocaleString stringKey="step_three.gamechanger.bullet.3" />
+            </li>
+          </ul>
         </CardContentCol>
       </CardContentRow>
-    </Card>
+    </CardProduct>
   );
 };
 
